a81a6af670d28f39bf36fb774b9ace97cbf7a349
Ken Atopos <z1158229761@outlook.com>
2023-02-28 11:48:16 +0000

    fix bug

--- market/__init__.py
+++ market/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from dotenv import load_dotenv
 import os
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config["SECRET_KEY"] = "5616c62c9b59226df2fa21a8"
+load_dotenv()
+app.config["SECRET_KEY"] = os.environ.get("MY_APP_SECRET_KEY")
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
